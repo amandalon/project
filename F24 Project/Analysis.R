f765bc9fbe9ad495bd4ca85bfd2751f681468c00
Ran0107 <rbran@ucdavis.edu>
2024-12-02 19:02:07 -0800

    Add files via upload
    Revised version

--- F24 Project/Analysis.R
+++ F24 Project/Analysis.R
@@ -1,6 +1,207 @@
+# omits any data that we won't be working with, along with any properties with a $0 sale price.AA
+library(readr)
+raw_data <- read_csv("Real_Estate_Sales_2001-2022_GL.csv")
+clean_data <- raw_data[raw_data$`Sale Amount` != 0, c('List Year', 'Date Recorded', 'Town', 'Sale Amount', 'Sales Ratio', 'Residential Type')]
+
+# sort data based on year.AA
+timeframe <- 2001:2022
+years <- function(year){
+  na.omit(clean_data[clean_data$'List Year' == year, ])
+} 
+
+# data sets by year.AA
+data_2001 <- years(2001)
+data_2002 <- years(2002)
+data_2003 <- years(2003)
+data_2004 <- years(2004)
+data_2005 <- years(2005)
+data_2006 <- years(2006)
+data_2007 <- years(2007)
+data_2008 <- years(2008)
+data_2009 <- years(2009)
+data_2010 <- years(2010)
+data_2011 <- years(2011)
+data_2012 <- years(2012)
+data_2013 <- years(2013)
+data_2014 <- years(2014)
+data_2015 <- years(2015)
+data_2016 <- years(2016)
+data_2017 <- years(2017)
+data_2018 <- years(2018)
+data_2019 <- years(2019)
+data_2020 <- years(2020)
+data_2021 <- years(2021)
+data_2022 <- years(2022)
+
+
+# residential types.AA
+residential_types <- unique(clean_data$`Residential Type`)
+residential_types
+
+# sort data based on residential type.AA
+sort_by_residential_type <- function(df, type){
+  df[df$`Residential Type` == type, ]
+} 
+
+data_by_year_residential <- list()
+
+# loop over each year and residential type, storing data frames in the nested list.AA
+for (year in timeframe) {
+  year_data <- years(year)
+  data_by_year_residential[[as.character(year)]] <- list()
+  
+  for (residential in residential_types) {
+    data_by_year_residential[[as.character(year)]][[residential]] <- sort_by_residential_type(year_data, residential)
+  }
+}
+
+# single family.AA
+single_2001 <- data_by_year_residential[["2001"]][["Single Family"]]
+single_2002 <- data_by_year_residential[["2002"]][["Single Family"]]
+single_2003 <- data_by_year_residential[["2003"]][["Single Family"]]
+single_2004 <- data_by_year_residential[["2004"]][["Single Family"]]
+single_2005 <- data_by_year_residential[["2005"]][["Single Family"]]
+single_2006 <- data_by_year_residential[["2006"]][["Single Family"]]
+single_2007 <- data_by_year_residential[["2007"]][["Single Family"]]
+single_2008 <- data_by_year_residential[["2008"]][["Single Family"]]
+single_2009 <- data_by_year_residential[["2009"]][["Single Family"]]
+single_2010 <- data_by_year_residential[["2010"]][["Single Family"]]
+single_2011 <- data_by_year_residential[["2011"]][["Single Family"]]
+single_2012 <- data_by_year_residential[["2012"]][["Single Family"]]
+single_2013 <- data_by_year_residential[["2013"]][["Single Family"]]
+single_2014 <- data_by_year_residential[["2014"]][["Single Family"]]
+single_2015 <- data_by_year_residential[["2015"]][["Single Family"]]
+single_2016 <- data_by_year_residential[["2016"]][["Single Family"]]
+single_2017 <- data_by_year_residential[["2017"]][["Single Family"]]
+single_2018 <- data_by_year_residential[["2018"]][["Single Family"]]
+single_2019 <- data_by_year_residential[["2019"]][["Single Family"]]
+single_2020 <- data_by_year_residential[["2020"]][["Single Family"]]
+single_2021 <- data_by_year_residential[["2021"]][["Single Family"]]
+single_2022 <- data_by_year_residential[["2022"]][["Single Family"]]
+
+# unknown.AA 
+unknown_2001 <- data_by_year_residential[["2001"]][["NA"]]
+unknown_2002 <- data_by_year_residential[["2002"]][["NA"]]
+unknown_2003 <- data_by_year_residential[["2003"]][["NA"]]
+unknown_2004 <- data_by_year_residential[["2004"]][["NA"]]
+unknown_2005 <- data_by_year_residential[["2005"]][["NA"]]
+unknown_2006 <- data_by_year_residential[["2006"]][["NA"]]
+unknown_2007 <- data_by_year_residential[["2007"]][["NA"]]
+unknown_2008 <- data_by_year_residential[["2008"]][["NA"]]
+unknown_2009 <- data_by_year_residential[["2009"]][["NA"]]
+unknown_2010 <- data_by_year_residential[["2010"]][["NA"]]
+unknown_2011 <- data_by_year_residential[["2011"]][["NA"]]
+unknown_2012 <- data_by_year_residential[["2012"]][["NA"]]
+unknown_2013 <- data_by_year_residential[["2013"]][["NA"]]
+unknown_2014 <- data_by_year_residential[["2014"]][["NA"]]
+unknown_2015 <- data_by_year_residential[["2015"]][["NA"]]
+unknown_2016 <- data_by_year_residential[["2016"]][["NA"]]
+unknown_2017 <- data_by_year_residential[["2017"]][["NA"]]
+unknown_2018 <- data_by_year_residential[["2018"]][["NA"]]
+unknown_2019 <- data_by_year_residential[["2019"]][["NA"]]
+unknown_2020 <- data_by_year_residential[["2020"]][["NA"]]
+unknown_2021 <- data_by_year_residential[["2021"]][["NA"]]
+unknown_2022 <- data_by_year_residential[["2022"]][["NA"]]
+
+# condo.AA
+condo_2001 <- data_by_year_residential[["2001"]][["Condo"]]
+condo_2002 <- data_by_year_residential[["2002"]][["Condo"]]
+condo_2003 <- data_by_year_residential[["2003"]][["Condo"]]
+condo_2004 <- data_by_year_residential[["2004"]][["Condo"]]
+condo_2005 <- data_by_year_residential[["2005"]][["Condo"]]
+condo_2006 <- data_by_year_residential[["2006"]][["Condo"]]
+condo_2007 <- data_by_year_residential[["2007"]][["Condo"]]
+condo_2008 <- data_by_year_residential[["2008"]][["Condo"]]
+condo_2009 <- data_by_year_residential[["2009"]][["Condo"]]
+condo_2010 <- data_by_year_residential[["2010"]][["Condo"]]
+condo_2011 <- data_by_year_residential[["2011"]][["Condo"]]
+condo_2012 <- data_by_year_residential[["2012"]][["Condo"]]
+condo_2013 <- data_by_year_residential[["2013"]][["Condo"]]
+condo_2014 <- data_by_year_residential[["2014"]][["Condo"]]
+condo_2015 <- data_by_year_residential[["2015"]][["Condo"]]
+condo_2016 <- data_by_year_residential[["2016"]][["Condo"]]
+condo_2017 <- data_by_year_residential[["2017"]][["Condo"]]
+condo_2018 <- data_by_year_residential[["2018"]][["Condo"]]
+condo_2019 <- data_by_year_residential[["2019"]][["Condo"]]
+condo_2020 <- data_by_year_residential[["2020"]][["Condo"]]
+condo_2021 <- data_by_year_residential[["2021"]][["Condo"]]
+condo_2022 <- data_by_year_residential[["2022"]][["Condo"]]
+
+# two family.AA
+two_fam_2001 <- data_by_year_residential[["2001"]][["Two Family"]]
+two_fam_2002 <- data_by_year_residential[["2002"]][["Two Family"]]
+two_fam_2003 <- data_by_year_residential[["2003"]][["Two Family"]]
+two_fam_2004 <- data_by_year_residential[["2004"]][["Two Family"]]
+two_fam_2005 <- data_by_year_residential[["2005"]][["Two Family"]]
+two_fam_2006 <- data_by_year_residential[["2006"]][["Two Family"]]
+two_fam_2007 <- data_by_year_residential[["2007"]][["Two Family"]]
+two_fam_2008 <- data_by_year_residential[["2008"]][["Two Family"]]
+two_fam_2009 <- data_by_year_residential[["2009"]][["Two Family"]]
+two_fam_2010 <- data_by_year_residential[["2010"]][["Two Family"]]
+two_fam_2011 <- data_by_year_residential[["2011"]][["Two Family"]]
+two_fam_2012 <- data_by_year_residential[["2012"]][["Two Family"]]
+two_fam_2013 <- data_by_year_residential[["2013"]][["Two Family"]]
+two_fam_2014 <- data_by_year_residential[["2014"]][["Two Family"]]
+two_fam_2015 <- data_by_year_residential[["2015"]][["Two Family"]]
+two_fam_2016 <- data_by_year_residential[["2016"]][["Two Family"]]
+two_fam_2017 <- data_by_year_residential[["2017"]][["Two Family"]]
+two_fam_2018 <- data_by_year_residential[["2018"]][["Two Family"]]
+two_fam_2019 <- data_by_year_residential[["2019"]][["Two Family"]]
+two_fam_2020 <- data_by_year_residential[["2020"]][["Two Family"]]
+two_fam_2021 <- data_by_year_residential[["2021"]][["Two Family"]]
+two_fam_2022 <- data_by_year_residential[["2022"]][["Two Family"]]
+
+# three family.AA
+three_fam_2001 <- data_by_year_residential[["2001"]][["Three Family"]]
+three_fam_2002 <- data_by_year_residential[["2002"]][["Three Family"]]
+three_fam_2003 <- data_by_year_residential[["2003"]][["Three Family"]]
+three_fam_2004 <- data_by_year_residential[["2004"]][["Three Family"]]
+three_fam_2005 <- data_by_year_residential[["2005"]][["Three Family"]]
+three_fam_2006 <- data_by_year_residential[["2006"]][["Three Family"]]
+three_fam_2007 <- data_by_year_residential[["2007"]][["Three Family"]]
+three_fam_2008 <- data_by_year_residential[["2008"]][["Three Family"]]
+three_fam_2009 <- data_by_year_residential[["2009"]][["Three Family"]]
+three_fam_2010 <- data_by_year_residential[["2010"]][["Three Family"]]
+three_fam_2011 <- data_by_year_residential[["2011"]][["Three Family"]]
+three_fam_2012 <- data_by_year_residential[["2012"]][["Three Family"]]
+three_fam_2013 <- data_by_year_residential[["2013"]][["Three Family"]]
+three_fam_2014 <- data_by_year_residential[["2014"]][["Three Family"]]
+three_fam_2015 <- data_by_year_residential[["2015"]][["Three Family"]]
+three_fam_2016 <- data_by_year_residential[["2016"]][["Three Family"]]
+three_fam_2017 <- data_by_year_residential[["2017"]][["Three Family"]]
+three_fam_2018 <- data_by_year_residential[["2018"]][["Three Family"]]
+three_fam_2019 <- data_by_year_residential[["2019"]][["Three Family"]]
+three_fam_2020 <- data_by_year_residential[["2020"]][["Three Family"]]
+three_fam_2021 <- data_by_year_residential[["2021"]][["Three Family"]]
+three_fam_2022 <- data_by_year_residential[["2022"]][["Three Family"]]
+
+#four family.AA
+four_fam_2001 <- data_by_year_residential[["2001"]][["Four Family"]]
+four_fam_2002 <- data_by_year_residential[["2002"]][["Four Family"]]
+four_fam_2003 <- data_by_year_residential[["2003"]][["Four Family"]]
+four_fam_2004 <- data_by_year_residential[["2004"]][["Four Family"]]
+four_fam_2005 <- data_by_year_residential[["2005"]][["Four Family"]]
+four_fam_2006 <- data_by_year_residential[["2006"]][["Four Family"]]
+four_fam_2007 <- data_by_year_residential[["2007"]][["Four Family"]]
+four_fam_2008 <- data_by_year_residential[["2008"]][["Four Family"]]
+four_fam_2009 <- data_by_year_residential[["2009"]][["Four Family"]]
+four_fam_2010 <- data_by_year_residential[["2010"]][["Four Family"]]
+four_fam_2011 <- data_by_year_residential[["2011"]][["Four Family"]]
+four_fam_2012 <- data_by_year_residential[["2012"]][["Four Family"]]
+four_fam_2013 <- data_by_year_residential[["2013"]][["Four Family"]]
+four_fam_2014 <- data_by_year_residential[["2014"]][["Four Family"]]
+four_fam_2015 <- data_by_year_residential[["2015"]][["Four Family"]]
+four_fam_2016 <- data_by_year_residential[["2016"]][["Four Family"]]
+four_fam_2017 <- data_by_year_residential[["2017"]][["Four Family"]]
+four_fam_2018 <- data_by_year_residential[["2018"]][["Four Family"]]
+four_fam_2019 <- data_by_year_residential[["2019"]][["Four Family"]]
+four_fam_2020 <- data_by_year_residential[["2020"]][["Four Family"]]
+four_fam_2021 <- data_by_year_residential[["2021"]][["Four Family"]]
+four_fam_2022 <- data_by_year_residential[["2022"]][["Four Family"]]
+
 # Merge data function.
 combine_data_by_year <- function(years_data) {
-  # Combine a single dataset.
+  # Combine a single data set.
   combined_data <- do.call(rbind, years_data)
   return(combined_data)
 }
@@ -12,7 +213,7 @@ single_family_data <- list(single_2001, single_2002, single_2003, single_2004, s
 
 single_family_combined <- combine_data_by_year(single_family_data)
 
-# Merge two-family data.
+# Merge two family data.
 two_family_data <- list(two_fam_2001, two_fam_2002, two_fam_2003, two_fam_2004, two_fam_2005, two_fam_2006, two_fam_2007,
                         two_fam_2008, two_fam_2009, two_fam_2010, two_fam_2011, two_fam_2012, two_fam_2013, two_fam_2014,
                         two_fam_2015, two_fam_2016, two_fam_2017, two_fam_2018, two_fam_2019, two_fam_2020, two_fam_2021, two_fam_2022)
@@ -37,7 +238,7 @@ four_family_combined <- combine_data_by_year(four_family_data)
 
 # Standardize column name formatting.
 adjust_colnames <- function(df) {
-  colnames(df) <- gsub(" ", "_", colnames(df))  
+  colnames(df) <- gsub(" ", "_", colnames(df)) 
   colnames(df) <- gsub("List_Year", "List_Year", colnames(df))  
   return(df)
 }
@@ -49,7 +250,7 @@ three_family_combined <- adjust_colnames(three_family_combined)
 four_family_combined <- adjust_colnames(four_family_combined)
 
 ###############################################################################
-# 1Analyze the factors affecting property sale prices
+# 1 Analyze the factors affecting property sale prices
 # 
 # 2 Identify the features influencing price (e.g., area, age of the property, renovation status, etc.).
 # 
@@ -65,16 +266,13 @@ four_family_combined <- adjust_colnames(four_family_combined)
 # 
 # 6 Analyze the listing duration of properties and compare sold and unsold properties to assess market liquidity.
 # 
-# 
-# 
-# 
+
 library(kableExtra)
 
-# (1) 
+# (1) Analyze the factors affecting property sale prices
 combined_data <- rbind(single_family_combined, two_family_combined, three_family_combined, four_family_combined)
 
 combined_data$Date_Recorded <- as.Date(combined_data$Date_Recorded, format = "%m/%d/%Y")
-
 
 head(combined_data$Date_Recorded)
 
@@ -83,18 +281,11 @@ print(correlation_analysis)
 lm_result <- lm(Sale_Amount ~ + Sales_Ratio  + Residential_Type, data = combined_data)
 summary(lm_result)
 
-# : 2.
-library(ggplot2)
-avg_sale_price_per_year <- aggregate(Sale_Amount ~ List_Year, data = combined_data, FUN = mean)
-ggplot(avg_sale_price_per_year, aes(x = List_Year, y = Sale_Amount)) +
-  geom_line() +
-  labs(title = "Average Sale Price Per Year", x = "Year", y = "Average Sale Price")
-
-
+# properties with Sales_Ratio < 1 are considered unsold, while those with Sales_Ratio == 1 are considered sold.
 sold_properties <- subset(combined_data, Sales_Ratio == 1)
 unsold_properties <- subset(combined_data, Sales_Ratio < 1)
 
-
+# Compare the differences between sold and unsold properties.
 summary_sold <- summary(sold_properties$Sale_Amount)
 summary_sold
 summary_unsold <- summary(unsold_properties$Sale_Amount)
@@ -111,14 +302,16 @@ sale_comparison %>%
   kable("html", col.names = c("Statistic", "Sold Properties", "Unsold Properties")) %>%
   kable_styling(bootstrap_options = c("striped", "hover", "condensed"))
 
-# Calculate the property turnover rate.
+# Property Turnover Rate
 town_turnover_rate <- aggregate(Sales_Ratio ~ Town, data = combined_data, FUN = function(x) sum(x == 1) / length(x))
 print(town_turnover_rate)
 town_turnover_rate %>%
   kable("html", col.names = c("Town", "Turnover Rate")) %>%
   kable_styling(bootstrap_options = c("striped", "hover", "condensed"))
 
-# Analyze property market liquidity.
+# Analyze the liquidity of the real estate market.
+# The listing duration is recorded in the Date_Recorded column.
+# Compute the average listing duration for sold and unsold properties.
 sold_properties$Date_Recorded <- as.Date(sold_properties$Date_Recorded, format = "%Y-%m-%d")
 unsold_properties$Date_Recorded <- as.Date(unsold_properties$Date_Recorded, format = "%Y-%m-%d")
 
@@ -136,3 +329,5 @@ inflation_data <- data.frame(
 
 combined_data <- merge(combined_data, inflation_data, by = "List_Year", all.x = TRUE)
 avg_inflation_adjusted_price_per_year <- aggregate(Inflation_Adjusted_Sale_Amount ~ List_Year, data = combined_data, FUN = mean)
+
+
